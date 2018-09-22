9206c0f4bb3a669563d453eda17e6c0b32666af3
Alexander Dominguez <alexrendon2109@gmail.com>
2018-09-22 16:21:49 -0500

    Added Signin button. Fixed navbar tag overflowing content.

---
+++ client/src/components/authentication/SignInButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SignInButton = () => {
+  return (
+    <button>Sign In</button>
+  );
+};
+
+export default SignInButton;
--- client/src/containers/Profile.js
+++ client/src/containers/Profile.js
@@ -1,39 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import AskUsername from '../components/askUsername/AskUsername';
 import axios from 'axios';
 import Leaderboard from './Leaderboard';
+import SignInButton from '../components/authentication/SignInButton';
 
 class Profile extends Component {
-  // constructor(props) {
-  //   super(props);
-
-  //   this.state = {
-  //     inputValue: null,
-  //   }
-
-  //   this.handleInputChange = this.handleInputChange.bind(this);
-  //   this.handleUsernameSubmission = this.handleUsernameSubmission.bind(this);
-  // }
-
-  // handleUsernameSubmission() {
-  //   axios.put('/api/set-username', { username: this.state.inputValue });
-  // }
-
-  // handleInputChange(event) {
-  //   this.setState({ inputValue: event.target.value });
-  // }
-
-  // renderAskUsername(profile) {
-  //   if (Object.keys(profile).length !== 0 && !profile.username) {
-  //     return <AskUsername
-  //       name={profile.name}
-  //       onUsernameSubmission={this.handleUsernameSubmission}
-  //       onInputChange={this.handleInputChange}
-  //     />
-  //   }
-  // }
-
   componentDidMount() {
     if (window.sessionStorage.scoreData) {
       const scoreData = JSON.parse(window.sessionStorage.scoreData);
@@ -42,11 +13,23 @@ class Profile extends Component {
     }
   }
 
+  renderLeaderboard(user) {
+    if (user) {
+      return <Leaderboard />
+    }
+  }
+
+  renderSignInButton(user) {
+    if (!user) {
+      return <SignInButton />
+    }
+  }
+
   render() {
     return (
       <div className="main-content">
-        {/* {this.renderAskUsername(this.props.auth)} */}
-        <Leaderboard />
+        {this.renderLeaderboard(this.props.auth)}
+        {this.renderSignInButton(this.props.auth)}
       </div>
     );
   }
