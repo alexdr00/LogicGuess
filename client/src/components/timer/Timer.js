1a216c4e76bc660912514fc2bd038cf439bb9979
Alexander Dominguez <alexrendon2109@gmail.com>
2018-09-22 23:29:55 -0500

    Added clock icon to timer.

--- client/src/components/timer/Timer.js
+++ client/src/components/timer/Timer.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import defineTheme from '../../lib/defineTheme';
 import formatTimeElapsed from '../../lib/formatTimeElapsed';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 const Timer = (props) => {
   return (
     <div className={`timer ${defineTheme(props.level)}`}>
+      <FontAwesomeIcon icon={['fas', 'clock']} className="timer__icon" />
+
       {formatTimeElapsed(props.timeElapsed)}
     </div>
   );
